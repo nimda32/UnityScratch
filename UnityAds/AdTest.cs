47748d12ec6b2fb3c09a9dd7e646a7d0b16a2575
--get-all <edmund0dao@gmail.com>
2018-07-21 21:01:22 -0700

    Added unityads example script.

---
+++ UnityAds/AdTest.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Advertisements;
+
+public class AdTest : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+        StartCoroutine(ShowAd());
+    }
+
+    public IEnumerator ShowAd()
+    {
+        while (!Advertisement.IsReady()) {
+            print("Waiting for Ads...");
+            yield return null;
+        }
+        Advertisement.Show();
+    }
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
---
+++ UnityAds/README.md
@@ -0,0 +1,3 @@
+# How to Abuse
+1. Services > EnableAds > EnableTestAds
+2. Attach script to any object > BOOM
